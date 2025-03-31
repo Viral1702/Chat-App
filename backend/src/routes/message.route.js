548f7cb7fbf029c7b5320716bb70e5b10e9e6036
Viral Shingadia <119102691+Viral1702@users.noreply.github.com>
2025-03-31 16:27:12 +0530

    Complited messages functionality

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
+
+const router = Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);
+
+router.get("/:id", protectRoute, getMessages);
+
+router.get("/send/:id", protectRoute, sendMessage);
+
+export default router;
