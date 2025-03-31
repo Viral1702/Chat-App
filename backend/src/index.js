aa2457f8fb89e22df7e45072e7b3cadce4baf352
Viral Shingadia <119102691+Viral1702@users.noreply.github.com>
2025-03-31 19:25:56 +0530

    Apply cors in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Coustoms Routes
 import authRoutes from "./routes/auth.route.js";
@@ -13,7 +14,12 @@ const app = express();
 // Default middlewares
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 // Coustom middlewares
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
