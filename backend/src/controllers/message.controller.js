71905ceefde5ec96d9331a909cc48e4b04b5d289
Viral Shingadia <viralshingadia38@gmail.com>
2026-03-31 10:44:52 +0530

    fix: remove secretcs from composefile

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -63,6 +63,7 @@ export const sendMessage = async (req, res) => {
 
     res.status(201).json(newMessage);
   } catch (error) {
+    console.error(error);
     console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+dotenv.config();
 
 // Coustoms Routes
 import authRoutes from "./routes/auth.route.js";
@@ -17,20 +18,19 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })
+  }),
 );
 
 // Coustom middlewares
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-server.listen(5001, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
